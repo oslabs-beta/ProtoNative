bee581c2eaed3fafc01855367754514d20816604
mkymn10 <mkymn10@g.ucla.edu>
2023-03-23 11:17:27 -0700

    get output structurefor parser

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,58 +1,3 @@
-const originals = {
-  app: { type: 'app', children: [], index: 0 },
-  view: { type: 'view', children: [], index: 0 },
-  button: { type: 'button', children: [], index: 0 },
-  text: { type: 'text', children: [], index: 0 },
-  image: { type: 'image', children: [], index: 0 },
-  textInput: { type: 'textInput', children: [], index: 0 },
-  scrollView: { type: 'scrollView', children: [], index: 0 },
-  flatList: { type: 'flatList', children: [], index: 0 },
-  sectionList: { type: 'sectionList', children: [], index: 0 },
-  switch: { type: 'switch', children: [], index: 0 },
-  touchableHighlight: { type: 'touchableHighlight', children: [], index: 0 },
-  touchableOpacity: { type: 'touchableOpacity', children: [], index: 0 },
-  statusBar: { type: 'statusBar', children: [], index: 0 },
-  activityIndicator: { type: 'activityIndicator', children: [], index: 0 },
-  testComponent: {
-    type: 'custom',
-    children: ['button1', 'view1'],
-    state: [],
-    index: 1,
-    copies: ['testComponent0'],
-  },
-}
-
-const copies = {
-  button0: {
-    type: 'button',
-    parent: { origin: 'original', key: 'testComponent' },
-    children: [],
-  },
-  text0: {
-    type: 'text',
-    parent: { origin: 'copies', key: 'view1' },
-    children: [],
-  },
-  view0: {
-    type: 'view',
-    parent: { origin: 'original', key: 'testComponent' },
-    children: ['text1'],
-  },
-  testComponent0: {
-    type: 'custom',
-    parent: 'app',
-    pointer: 'testComponent',
-    children: function() {
-      return originals[this.pointer].children;
-    },
-    state: function() {
-      return originals[this.pointer].state;
-    }
-  },
-};
-
-console.log(copies.testComponent0.children());
-
 import {
   NativeElements,
   OrigNativeEl,
@@ -63,6 +8,127 @@ import {
   CopyCustomComp
 } from './interfaces';
 
-const generateReactNativeCode = () => {
+const originals = {
+  app: { type: 'app', children: ['testComponent0', 'coolComponent0', 'view0'], state: [] } as AppInterface,
+  view: { type: 'view', children: [], index: 0 } as OrigNativeEl,
+  button: { type: 'button', children: [], index: 0 } as OrigNativeEl,
+  text: { type: 'text', children: [], index: 0 } as OrigNativeEl,
+  image: { type: 'image', children: [], index: 0 } as OrigNativeEl,
+  textInput: { type: 'textInput', children: [], index: 0 } as OrigNativeEl,
+  scrollView: { type: 'scrollView', children: [], index: 0 } as OrigNativeEl,
+  flatList: { type: 'flatList', children: [], index: 0 } as OrigNativeEl,
+  sectionList: { type: 'sectionList', children: [], index: 0 } as OrigNativeEl,
+  switch: { type: 'switch', children: [], index: 0 } as OrigNativeEl,
+  touchableHighlight: { type: 'touchableHighlight', children: [], index: 0 } as OrigNativeEl,
+  touchableOpacity: { type: 'touchableOpacity', children: [], index: 0 } as OrigNativeEl,
+  statusBar: { type: 'statusBar', children: [], index: 0 } as OrigNativeEl,
+  activityIndicator: { type: 'activityIndicator', children: [], index: 0 } as OrigNativeEl,
+  testComponent: {
+    name: 'testComponent',
+    type: 'custom',
+    children: ['button0', 'view0', 'coolComponent0'],
+    state: ['testState'],
+    index: 1,
+    copies: ['testComponent0'],
+  } as OrigCustomComp,
+  coolComponent: {
+    name: 'coolComponent',
+    type: 'custom',
+    children: ['button2', 'view1'],
+    state: [],
+    index: 1,
+    copies: ['testComponent0'],
+  } as OrigCustomComp
+}
+
+const copies = {
+  button0: {
+    name: 'button0',
+    type: 'button',
+    parent: { origin: 'original', key: 'testComponent' },
+    children: [],
+  } as CopyNativeEl,
+  text0: {
+    name: 'text0',
+    type: 'text',
+    parent: { origin: 'copies', key: 'view0' },
+    children: ['button1'],
+  } as CopyNativeEl,
+  view0: {
+    name: 'view0',
+    type: 'view',
+    parent: { origin: 'original', key: 'testComponent' },
+    children: ['text0'],
+  } as CopyNativeEl,
+  button1: {
+    name: 'button1',
+    type: 'button',
+    parent: { origin: 'original', key: 'text0' },
+    children: [],
+  } as CopyNativeEl,
+  view1: {
+    name: 'view1',
+    type: 'view',
+    parent: { origin: 'original', key: 'coolComponent' },
+    children: ['text0'],
+  } as CopyNativeEl,
+  button2: {
+    name: 'button2',
+    type: 'button',
+    parent: { origin: 'original', key: 'coolComponent' },
+    children: [],
+  } as CopyNativeEl,
+  testComponent0: {
+    name: 'testComponent0',
+    type: 'custom',
+    parent: 'app',
+    pointer: 'testComponent',
+    children: function() {
+      return originals[this.pointer].children;
+    },
+    state: function() {
+      return originals[this.pointer].state;
+    }
+  } as CopyCustomComp,
+  coolComponent0: {
+    name: 'coolComponent0',
+    type: 'custom',
+    parent: 'app',
+    pointer: 'coolComponent',
+    children: function() {
+      return originals[this.pointer].children;
+    },
+    state: function() {
+      return originals[this.pointer].state;
+    }
+  } as CopyCustomComp,
+}
+
+console.log(copies.testComponent0.children());
+
+/*
+---OUTPUT FOR TEST COMPONENT---
+
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import coolComponent from './components/coolComponent';
+
+const testComponent = () => {
+  const [testState, setTestState] = React.useState(null);
+
+  return (
+    <>
+      <Button />
+      <View>
+        <Text>
+          <Button />
+        </Text>
+      </View>
+    </>
+  )
+};
+*/
+
+const generateCustomComponentCode = (component) => {
   
 }
