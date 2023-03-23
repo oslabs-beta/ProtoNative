d3d560c118c24b243af8d1cf35f475803467825e
Michelle Leong <leong.michellew@gmail.com>
2023-03-23 10:28:31 -0700

    starting

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from 'react';
 import LeftContainer from '../left/LeftContainer';
-import AppCanvas from '../middle/AppCanvas'
-import RightContainer from '../right/RightContainer'
-
+import AppCanvas from '../middle/AppCanvas';
+import RightContainer from '../right/RightContainer';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 
 const MainContainer = (): JSX.Element => {
   return (
-    <div id='main-container'>
-      <LeftContainer />
-      <AppCanvas />
-      <RightContainer />
-    </div>
-  )
-}
+    <DndProvider backend={HTML5Backend}>
+      <div id='main-container'>
+        <LeftContainer />
+        <AppCanvas />
+        <RightContainer />
+      </div>
+    </DndProvider>
+  );
+};
 
 export default MainContainer;
