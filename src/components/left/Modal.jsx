fc407c4050479c4368c9fdd35b0502f145345497
Raymond Ferrer <rnferrer@ucdavis.edu>
2023-03-23 19:04:30 -0700

    allow for modal to close and removed script tag in index

--- src/components/left/Modal.jsx
+++ src/components/left/Modal.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
 
-const Modal = () => {
+const Modal = ({location = 'delete'}) => {
   const [isOpen, setIsOpen] = useState(false);
   const handleClick = (e) => {
     console.log('clicked')
@@ -12,7 +12,15 @@ const Modal = () => {
       display: isOpen ? 'none' : 'flex'
     }}>
       <div id='actual-modal'>
-        <div id='close-button' onClick={()=>{console.log('hello')}}>X</div>
+        <div id='close-button' onClick={handleClick}>X</div>
+        {location === 'delete'
+        ? 
+        <div>
+          <p>Are you sure you want to delete this component?</p>
+        </div>
+        :
+        <p>Edit State:</p>
+        }
       </div>
     </div>
   )
