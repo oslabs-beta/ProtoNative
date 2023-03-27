dc352ed93ee9e949ad1393b2e353b0e47a1c4d14
Raymond Ferrer <rnferrer@ucdavis.edu>
2023-03-27 11:29:38 -0700

    added styling

--- src/components/right/RightContainer.tsx
+++ src/components/right/RightContainer.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ComponentDetails from './ComponentDetails';
 import CodeBlock from './CodeBlock';
+import AddComponent from '../left/AddComponent';
 
 
 const RightContainer = (): JSX.Element => {
   return (
     <div id='right-container'>
       <ComponentDetails />
+      <AddComponent/>
       <CodeBlock />
     </div>
   )
