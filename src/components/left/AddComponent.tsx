857d34d6e04baef26d4e64d645dda22024e83bfd
Raymond Ferrer <rnferrer@ucdavis.edu>
2023-03-30 08:57:44 -0700

    styling add component modal

--- src/components/left/AddComponent.tsx
+++ src/components/left/AddComponent.tsx
@@ -55,21 +55,20 @@ const AddComponent = () => {
         Add Custom Component 
       </button>
       {isOpen && (
-        <div id="modal">
-          <Modal handleClick={()=> setIsOpen(false)}>
-            <div id="modal-content">
-              <h2>Add Custom Component</h2>
-              <form onSubmit={handleSubmit}>
-                <label>
-                  Component Name:
-                  <input type="text" value={componentName} onChange={handleInputChange} />
-                </label>
-                <button type="submit">Add</button>
-                <button onClick={handleClose}>Cancel</button>
-              </form>
-            </div>
-          </Modal>
-        </div>
+        <Modal handleClick={()=> setIsOpen(false)}>
+          <div id="modal-content">
+            <h2>Add Custom Component</h2>
+            <form onSubmit={handleSubmit}>
+              <label className='add-component-label'>
+                Component Name:
+              </label>
+              <input id='add-component-input' type="text" value={componentName} onChange={handleInputChange} placeholder='Component Name'/>
+
+              <button type="submit">Add</button>
+              <button onClick={handleClose}>Cancel</button>
+            </form>
+          </div>
+        </Modal>
       )}
     </div>
   );
